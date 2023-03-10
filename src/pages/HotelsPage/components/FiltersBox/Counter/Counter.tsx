0e2c0f2760a086f686e21c4c108035d045dd6d90
Marta Małachowicz <marta@macbook-air.home>
2023-03-10 08:26:29 +0000

    add missing package

--- src/pages/HotelsPage/components/FiltersBox/Counter/Counter.tsx
+++ src/pages/HotelsPage/components/FiltersBox/Counter/Counter.tsx
@@ -8,18 +8,18 @@ type Props = {
 };
 
 type ButtonProps = {
-  name: '+' | '-';
+  label: '+' | '-';
   handleClick: () => void;
 };
 
-const Button = React.memo(({ handleClick, name }: ButtonProps) => {
+const Button = React.memo(function Button({ handleClick, label }: ButtonProps) {
   return (
     <button
       className="text-gray-600 cursor-pointer hover:text-gray-700"
       onClick={handleClick}
       type="button"
     >
-      {name}
+      {label}
     </button>
   );
 });
@@ -28,9 +28,9 @@ function Counter({ label, count, handleIncrement, handleDecrement }: Props) {
   return (
     <div className="flex items-center">
       <div className="pr-2">{label}:</div>
-      <Button name="-" handleClick={() => handleDecrement()} />
+      <Button label="-" handleClick={() => handleDecrement()} />
       <div className="px-2">{count}</div>
-      <Button name="+" handleClick={() => handleIncrement()} />
+      <Button label="+" handleClick={() => handleIncrement()} />
     </div>
   );
 }
